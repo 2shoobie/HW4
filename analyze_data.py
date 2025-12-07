6f07dc69588646cdbfb589338a7ea80841fa9a62
SoliiMbu <70231735+2shoobie@users.noreply.github.com>
2025-12-07 15:58:32 -0500

    Refactor comments and improve code structure

--- analyze_data.py
+++ analyze_data.py
@@ -9,7 +9,7 @@ It gives the initial rows of the data, summary data and statistics. Creation and
 All the findings are printed on the console.
 """
 import pandas as pd
-
+#1 Data Loading 
 df = pd.read_csv("data/StudentExam.csv")
 
 print("\n First 5 rows of the DataFrame:")
@@ -20,7 +20,7 @@ print(df.info())
 
 print("\n Summary Statistics:")
 print(df.describe())
-# 2  
+# 2  Indexing and Selection
 print("2: A single row by its position")
 print(df.iloc[0])
 
@@ -35,7 +35,7 @@ print(df["reading score"].head())
 
 print("\n Single cell with loc[0, 'reading score']")
 print(df.loc[0, "reading score"])
-# 3
+# 3 Filtering
 print ("\n Students with reading score greater than 90:")
 high_reading = df[df["reading score"] > 90]
 print(high_reading)
@@ -43,7 +43,7 @@ print(high_reading)
 print("\n Male students with reading score less than 50:")
 male_reading_level = df[(df["reading score"] < 50) & (df["gender"] == "male")]
 print(male_reading_level.head())
-# 4
+# 4 Droping and Creating Columns
 print("\n average score:")
 df["average score"] = df[["math score", "reading score", "writing score"]].mean(axis=1) /3
 print(df[["reading score", "math score", "writing score"]].head())
@@ -52,7 +52,7 @@ print("reading score Drop Column:")
 df = df.drop("reading score", axis=1)
 print("\nCurrent Columns:")
 print(df.columns)
-# 5
+# 5 GroupBy Operations
 print("average writing score by gender:")
 gender_scre = df.groupby("gender")["writing score"].mean()
 print(gender_scre)
